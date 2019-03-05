eac250a433f465b504abeed71410dfd53dc71ed9
Gaige Kinsey <gaigekinsey@yahoo.com>
2019-03-05 10:52:41 -0700

    Added ability to put in number for the board

--- sudoku/src/main/java/edu/neumont/csc150/sudoku/view/sudokugame/SudokuGameViewController.java
+++ sudoku/src/main/java/edu/neumont/csc150/sudoku/view/sudokugame/SudokuGameViewController.java
@@ -9,29 +9,30 @@ import edu.neumont.csc150.sudoku.controller.SudokuController;
 import edu.neumont.csc150.sudoku.view.SudokuViewController;
 import javafx.css.PseudoClass;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckMenuItem;
 import javafx.scene.control.Label;
-import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.KeyCode;
+import javafx.scene.input.KeyEvent;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.scene.paint.Color;
 import javafx.stage.FileChooser;
 
 public class SudokuGameViewController {
-	
+
 	private SudokuViewController mainView;
 	private SudokuController controller;
-	
+
 	private Map<String, Label> cells = new HashMap<>();
-	
+
 	@FXML
 	private GridPane sudokuBoard;
-	
-	public void click(ActionEvent e) {
-		mainView.click(e);
-	}
-	
+
 	public void onSave(ActionEvent e) {
 		boolean saved = false;
 		do {
@@ -52,57 +53,86 @@ public class SudokuGameViewController {
 			}
 		} while (!saved);
 	}
+
 	public void onClear(ActionEvent e) {
 		controller.clearBoard();
 		displayBoard();
 	}
-	
+
 	public void onNewPuzzle(ActionEvent e) {
 		mainView.loadBoard(mainView.getDifficulty());
 	}
-	
+
 	public void onMainMenu(ActionEvent e) {
 		mainView.showMainMenu();
 	}
-	
+
 	public void onToggled(ActionEvent e) {
 		CheckMenuItem notesBox = (CheckMenuItem) e.getSource();
 		if (notesBox.isSelected()) {
-			// Method call to display the notes in empty boxes
+			// Method call to display the notes in empty boxes TODO
 
 		} else {
 			// Method call to display normal board
 			displayBoard();
 		}
 	}
-	
+
 	public void drawBoard() {
-		
+
 		PseudoClass right = PseudoClass.getPseudoClass("right");
 		PseudoClass bottom = PseudoClass.getPseudoClass("bottom");
-		
-		for(int col=0; col<9; col++) {
-			for(int row=0; row<9; row++) {
+
+		for (int col = 0; col < 9; col++) {
+			for (int row = 0; row < 9; row++) {
 				Label cell = new Label();
 				cell.getStyleClass().add("cell");
 				cell.pseudoClassStateChanged(right, col == 2 || col == 5);
 				cell.pseudoClassStateChanged(bottom, row == 2 || row == 5);
-				
+				if (!controller.getBoard().getSquares()[col][row].isHint()) {
+					cell.setOnMouseClicked(new EventHandler<MouseEvent>() {
+
+						@Override
+						public void handle(MouseEvent mouseEvent) {
+							cell.requestFocus();
+							cell.setOnKeyPressed(new EventHandler<KeyEvent>() {
+
+								@Override
+								public void handle(KeyEvent keyEvent) {
+									if (keyEvent.getCode().isDigitKey()) {
+										cell.setTextFill(Color.DARKCYAN);
+										controller.getBoard().setSquare(Integer.parseInt(cell.getId().split("x")[0]),
+												Integer.parseInt(cell.getId().split("x")[1]),
+												Integer.parseInt(keyEvent.getCode().getName()));
+									}
+									if (keyEvent.getCode() == KeyCode.BACK_SPACE) {
+										controller.getBoard().setSquare(Integer.parseInt(cell.getId().split("x")[0]),
+												Integer.parseInt(cell.getId().split("x")[1]), 0);
+									}
+									displayBoard();
+								}
+							});
+						}
+					});
+				}
+
 				cell.setId("" + col + "x" + row);
 				this.cells.put(cell.getId(), cell);
 				this.sudokuBoard.add(cell, col, row);
 			}
 		}
+
 		displayBoard();
+
 	}
-	
+
 	public void displayBoard() {
-		for(int col=0; col<9; col++) {
-			for(int row=0; row<9; row++) {
+		for (int col = 0; col < 9; col++) {
+			for (int row = 0; row < 9; row++) {
 				Label cell = cells.get("" + col + "x" + row);
 				int num = controller.getBoard().getSquares()[col][row].getValue();
 				if (num != 0) {
-					cell.setText("" + num);					
+					cell.setText("" + num);
 				} else {
 					cell.setText("  ");
 				}
