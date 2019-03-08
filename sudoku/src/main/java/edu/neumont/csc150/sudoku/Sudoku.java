c3100842a9e71fedfa2fc4b560cc005e62a2a9b1
WistyDagel <chrisdazley@gmail.com>
2019-03-08 00:18:53 -0700

    Added songs to the view package

--- sudoku/src/main/java/edu/neumont/csc150/sudoku/Sudoku.java
+++ sudoku/src/main/java/edu/neumont/csc150/sudoku/Sudoku.java
@@ -3,6 +3,8 @@ package edu.neumont.csc150.sudoku;
 import edu.neumont.csc150.sudoku.controller.SudokuController;
 import edu.neumont.csc150.sudoku.view.SudokuViewController;
 import javafx.application.Application;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
@@ -18,6 +20,9 @@ public class Sudoku extends Application{
 		viewController.setStage(stage);
 		SudokuController controller = new SudokuController(viewController);
 		Font.loadFont(getClass().getResourceAsStream("/edu/neumont/csc150/sudoku/view/lastninja.ttf"), 25);
+//		Media media = new Media("/edu/neumont/csc150/sudoku/view/village.mp3");
+//		MediaPlayer player = new MediaPlayer(media);
+//		player.play();
 		controller.run();
 	}
 }
