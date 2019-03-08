a6c98b48084a90a775983cdf82a1614bd3ec9e83
WistyDagel <chrisdazley@gmail.com>
2019-03-08 08:43:27 -0700

    Music can now play

--- sudoku/src/main/java/edu/neumont/csc150/sudoku/Sudoku.java
+++ sudoku/src/main/java/edu/neumont/csc150/sudoku/Sudoku.java
@@ -1,5 +1,7 @@
 package edu.neumont.csc150.sudoku;
 
+import java.nio.file.Paths;
+
 import edu.neumont.csc150.sudoku.controller.SudokuController;
 import edu.neumont.csc150.sudoku.view.SudokuViewController;
 import javafx.application.Application;
@@ -9,6 +11,7 @@ import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Sudoku extends Application{
+	MediaPlayer player;
 
 	public static void main(String[] args) {
 		Application.launch(Sudoku.class, args);
@@ -20,9 +23,10 @@ public class Sudoku extends Application{
 		viewController.setStage(stage);
 		SudokuController controller = new SudokuController(viewController);
 		Font.loadFont(getClass().getResourceAsStream("/edu/neumont/csc150/sudoku/view/lastninja.ttf"), 25);
-//		Media media = new Media("/edu/neumont/csc150/sudoku/view/village.mp3");
-//		MediaPlayer player = new MediaPlayer(media);
-//		player.play();
+		Media media = new Media(this.getClass().getResource("/edu/neumont/csc150/sudoku/view/village.mp3").toURI().toString());		
+		player = new MediaPlayer(media);
+		player.setCycleCount(MediaPlayer.INDEFINITE);
+		player.play();
 		controller.run();
 	}
 }
